a0b82d8b0b55afc761537ed7657612d78ff49bf9
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-10-25 23:14:32 +0200

    begin routes for clacks

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var clicksRouter = require('./routes/clicks');
 var clacksRouter = require('./routes/clacks');
+var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var commandsRouter = require('./routes/commands');
 
 var app = express();
 
@@ -22,9 +24,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/clicks', clicksRouter);
 app.use('/clacks', clacksRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/commands', commandsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/clacks.js
+++ routes/clacks.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
-/* GET users listing. */
+/* GET clacks listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  request.get({ url: "http://localhost:8080/clacks/" }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      publicBody = JSON.parse(body);
+      publicBody.forEach((clack)=>{
+        delete clack._id;
+      });
+      res.render( 'clacks', { "clacks": publicBody } );
+    } else {
+      res.render('error', {
+        "error": {
+          "stack": error,
+          "status": 404
+        },
+        "message": "Unable to find clacks"
+      })
+    }
+  });
 });
 
 module.exports = router;
